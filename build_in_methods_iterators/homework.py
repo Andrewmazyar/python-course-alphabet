a29458cd40fe4176479ef608cc68b6f10903ad5f
Andrewmazyar <maziarandriy@gmail.com>
2019-04-23 16:18:54 +0300

    python_functionality

--- build_in_methods_iterators/homework.py
+++ build_in_methods_iterators/homework.py
@@ -1,4 +1,6 @@
 from typing import List, Dict, Union, Generator
+import random
+import string
 
 # We will work with such dicts
 ST = Dict[str, Union[str, int]]
@@ -9,23 +11,27 @@ DT = List[ST]
 def task_1_fix_names_start_letter(data: DT) -> DT:
     """
     Make all `names` field in list of students to start from upper letter
-
     Examples:
         fix_names_start_letters([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}])
         >>> [{'name': 'Alex', 'age': 26}, {'name': 'Denys', 'age': 89}]
     """
-    pass
+    for x in data:
+        x['name'] = x['name'].capitalize()
+    return data
 
 
 def task_2_remove_dict_fields(data: DT, redundant_keys: List[str]) -> DT:
     """given_data
     Remove from dictionaries given key value
-
     Examples:
        remove_dict_field([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}], 'age')
         >>> [{'name': 'Alex'}, {'name': 'denys'}]
     """
-    pass
+    for x in data:
+        for key in redundant_keys:
+            if key in x:
+                del x[key]
+    return data
 
 
 def task_3_find_item_via_value(data: DT, value) -> DT:
@@ -35,44 +41,69 @@ def task_3_find_item_via_value(data: DT, value) -> DT:
         find_item_via_value([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}], 26)
         >>> [{'name': 'Alex', 'age': 26}]
     """
-    pass
-
+  #  for x in data:
+  #      if x.values() == value:
+   #         return data[x]
+    return [d for d in data if value in d.values()]
 
 def task_4_min_value_integers(data: List[int]) -> int:
     """
     Find and return minimum value from list
     """
-    pass
+    x = int(data[0])
+    for i in data:
+        if int(i) < x:
+            x = i
+    return int(x)
 
 
 def task_5_min_value_strings(data: List[Union[str, int]]) -> str:
     """
     Find the longest string
     """
-    pass
+    some = ''
+    l = 0
+    for x, y in data:
+        if len(x) > l:
+            l = len(x)
+            some = x
+    return some
+
 
 
 def task_6_min_value_list_of_dicts(data: DT, key: str) -> ST:
     """
     Find minimum value by given key
     Returns:
-
     """
-    pass
+    for x in data:
+        pass
+    #some code
+
+
+
 
 
 def task_7_max_value_list_of_lists(data: List[List[int]]) -> int:
     """
     Find max value from list of lists
     """
-    pass
-
+  #  x = data[0]
+   # for i in data:
+    #    if i > x:
+     #       x = i
+    #return x
+    return max(list(map(lambda x: max(x), data)))
 
 def task_8_sum_of_ints(data: List[int]) -> int:
     """
     Find sum of all items in given list
     """
-    pass
+    l=0
+    for x in data:
+        l+=x
+    return l
+
 
 
 def task_9_sum_characters_positions(text: str) -> int:
@@ -80,15 +111,16 @@ def task_9_sum_characters_positions(text: str) -> int:
     Please read first about ascii table.
     https://python-reference.readthedocs.io/en/latest/docs/str/ASCII.html
     You need to calculate sum of decimal value of each symbol in text
-
     Examples:
         task_9_sum_characters_positions("A")
         >>> 65
         task_9_sum_characters_positions("hello")
         >>> 532
-
     """
-    pass
+    sum = 0
+    for x in text:
+        sum +=ord(x)
+    return sum
 
 
 def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
@@ -102,12 +134,23 @@ def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
         next(a)
         >>> 3
     """
-    pass
+    fibonacci = []
+    fib0 = 1
+    fib1 = 1
+    while fib1<=200:
+        fibonacci.append(fib1)
+        fibsum = fib1 + fib0
+        fib0 =fib1
+        fib1 = fibsum
+    return fibonacci
+
 
 
 def task_11_create_list_of_random_characters() -> List[str]:
     """
     Create list of 20 elements where each element is random letter from latin alphabet
-
     """
-    pass
+    l = []
+    for x in range(20):
+        l.append(random.choice(string.ascii_letters))
+    return l
